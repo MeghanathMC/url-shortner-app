0cfbe0369f7ff2871699f4dfab9a746882d05363
MeghanathMC <meghanath.mc24@gmail.com>
2025-09-28 14:36:12 +0530

    url added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
    theme: {
      extend: {
       backgroundImage: { 
-       banner: "url('./src/assets/banner.webp')"
+       banner: "url('/banner.webp')"
       }
      },
    },
