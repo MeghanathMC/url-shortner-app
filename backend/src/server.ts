e98972248148d457e7d25140520ab4450cc91dfa
MeghanathMC <meghanath.mc24@gmail.com>
2025-09-28 13:50:29 +0530

    frotnend url added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors(
   {
-        origin:"http://localhost:5173",
+        origin: [
+          "http://localhost:5173",
+        "https://url-shortner-app-pearl.vercel.app/",
+        ],
         credentials:true
     }
 ));
